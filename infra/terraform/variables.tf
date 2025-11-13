c367e07d1bfcf4ca1ef3c413d4393d3c262b17f3
iamnkit <73685609+iamnkit@users.noreply.github.com>
2025-11-13 18:59:09 +0530

    Update variables.tf

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -1,1 +1,34 @@
+variable "prefix" {
+  description = "Prefix for resource names"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region"
+  type        = string
+  default     = "East US"
+}
+
+variable "vm_size" {
+  description = "Size of the VM"
+  type        = string
+  default     = "Standard_DS1_v2"
+}
+
+variable "admin_username" {
+  description = "Admin username for VM"
+  type        = string
+  default     = "azureuser"
+}
+
+variable "admin_password" {
+  description = "Admin password for VM"
+  type        = string
+}
+
+variable "os_disk_size" {
+  description = "OS disk size in GB"
+  type        = number
+  default     = 30
+}
 
