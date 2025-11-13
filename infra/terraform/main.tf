24d86b95e29f140aff742180e6ce3cf8cdceb03e
iamnkit <73685609+iamnkit@users.noreply.github.com>
2025-11-13 15:30:32 +0530

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -1,1 +1,31 @@
+module "network" {
+  source              = "./modules/network"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  vnet_name           = "${var.prefix}-vnet"
+  subnet_name         = "${var.prefix}-subnet"
+}
+
+module "acr" {
+  source              = "./modules/acr"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  prefix              = var.prefix
+}
+
+module "aks" {
+  source              = "./modules/aks"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  vnet_subnet_id      = module.network.subnet_id
+  acr_id              = module.acr.acr_id
+  prefix              = var.prefix
+}
+
+module "keyvault" {
+  source              = "./modules/keyvault"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  prefix              = var.prefix
+}
 
