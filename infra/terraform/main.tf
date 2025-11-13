b3df3144de3440eb66d5d4a9d111f841f9348609
iamnkit <73685609+iamnkit@users.noreply.github.com>
2025-11-13 19:02:37 +0530

    Update main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -36,3 +36,32 @@ module "storage" {
 }
 
 
+# Virtual Machine
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "${var.prefix}-vm"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = var.location
+  size                = var.vm_size
+  admin_username      = var.admin_username
+  admin_password      = var.admin_password
+  network_interface_ids = [
+    azurerm_network_interface.nic.id
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+    disk_size_gb         = var.os_disk_size
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "22.04-LTS"
+    version   = "latest"
+  }
+
+  disable_password_authentication = false
+}
+
+
